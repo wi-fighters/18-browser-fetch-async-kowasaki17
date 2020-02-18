2297e92c5950275e24c101f51b4fd6185d97cf41
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2020-02-18 11:41:00 +0100

    Solved the exercise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,15 +2,50 @@ import "../styles/main.scss";
 import "babel-polyfill"
 
 const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
+const btn = document.getElementById("continue");
+
+
+function resolveModal() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+  
+async function asyncCall() {
+    const result = await resolveModal();
+    console.log("User has been on the page for 60 seconds");
+    modal.style.display = "block";
+  }
+asyncCall();
+
+/* let promiseOfModal = new Promise(function (resolve) {
     window.setTimeout(function () {
         resolve(modal)
     }, (1000 * 60));
-});
-
-promiseOfModal.then(function(val) {
+}); */
+/* promiseOfModal.then(function(val) {
     console.log("User has been on the page for 60 seconds");
     val.style.display = "block";
+}) */
+
+const btnMsg = "Continue to subscribe"
+
+btn.addEventListener("mouseout", (e) => {
+    function resolveMsg() {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve('resolved');
+          }, 200);
+        });
+      }
+      async function asyncCall() {
+        const result = await resolveMsg();
+        console.log("Change button text");
+        btn.textContent = btnMsg;
+      }
+      asyncCall();
 })
 
 modal.addEventListener("click", (e) => {
